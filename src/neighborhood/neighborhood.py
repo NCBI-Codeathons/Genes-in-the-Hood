83b2b096a5c7fb847b46d8cbc132c6da7175b560
schuler <schuler@ncbi.nlm.nih.gov>
2020-07-08 22:40:05 -0400

    argument parsing

--- src/neighborhood/neighborhood.py
+++ src/neighborhood/neighborhood.py
@@ -4,30 +4,55 @@ import zipfile
 
 
 class ThisApp:
-    input_path = os.path.join('var', 'data')
-    output_path = os.path.join('var', 'data', 'neighborhood')
-    gene = 'cas9'
+    default_input_path = os.path.join('var', 'data', 'packages')
+    default_output_path = os.path.join('var', 'data', 'neighborhood')
+    default_gene = 'cas9'
+    defult_window_bp = 10000
+
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-I', '--input-path', type=str, default=self.default_input_path,
+                            help=f'root of input data directory [{self.default_input_path}]')
+        parser.add_argument('-O', '--output-path', type=str, default=self.default_output_path,
+                            help=f'root of output data directory [{self.default_output_path}]')
+        parser.add_argument('-g', '--gene', type=str, default=self.default_gene,
+                            help=f'gene symbol [{self.default_gene}]')
+        parser.add_argument('-w', '--window', type=int, default=self.defult_window_bp,
+                            help=f'gene symbol [{self.defult_window_bp}]')
+        self.args = parser.parse_args()
 
     def run(self):
-        zip_files = self.get_zip_files(self.input_path)
-        for zip_file in zip_files:
-            try:
-                with zipfile.ZipFile(zip_file, 'r') as zin:
-                    print(f'Reading {zip_file} ...')
-            except zipfile.BadZipFile:
-                print(f'{zip_file} is not a zip file')
+        zip_files = self.get_zip_files(self.args.input_path)
+        self.process_zip_files(zip_files)
 
     def get_zip_files(self, path):
-        """
-        Get a list of ZIP packages, each expected to contain ncbi_dataset for one NCBI assembly
-
-        :param path: str - root of input directory
-        :return: list of str - file paths for discovered ZIP files
-        """
         zip_files = []
         for root, dirs, files in os.walk(path):
             zip_files += [os.path.join(root, x) for x in files if x.endswith('.zip')]
         return zip_files
+
+    def process_zip_files(self, zip_files):
+        print(f'Processing {len(zip_files)} assemblies ...')
+        for zip_file in zip_files:
+            # print(zip_file)
+            acc = self.get_accession(zip_file)
+            try:
+                with zipfile.ZipFile(zip_file, 'r') as zin:
+                    gff_lines = self.read_file_lines(zin, f'ncbi_dataset/data/{acc}/genomic.gff')
+            except zipfile.BadZipFile:
+                print(f'{zip_file} is not a zip file')
+
+    def get_accession(self, file_name):
+        # For now, extract from filename,  Ideally, it should come from a file within
+        # the package, like ncbi_dataset/data/dataset_catalog.json
+        parts = os.path.split(file_name)
+        x = parts[-1].split('.')
+        return f'{x[0]}.{x[1]}'
+
+    def read_file_lines(self, zin, file_name):
+        print(f'Extracting {file_name}')
+        with zin.open(file_name) as fin:
+            return fin.read()
 
 
 if __name__ == '__main__':
